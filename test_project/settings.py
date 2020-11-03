b355004ddec90045dad09993b091f8f4a0b72e1e
mozgovoy <nikitafeodosia@gmail.com>
2020-11-03 19:41:33 +0300

    static

--- test_project/settings.py
+++ test_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
-    'admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
