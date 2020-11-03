e537f355df9fa5a75b2e2f32dc47ff525c3c88cb
mozgovoy <nikitafeodosia@gmail.com>
2020-11-03 18:23:51 +0300

    static

--- test_project/settings.py
+++ test_project/settings.py
@@ -122,5 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
+    '/usr/lib/python3/dist-packages/django/contrib/admin/'
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
