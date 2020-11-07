6fb71339115e46e16c479e191c1526ab1cdf3f43
mozgovoy <nikitafeodosia@gmail.com>
2020-11-07 22:28:33 +0300

    tests

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 
-class MainPageView(TemplateView):
+class MainPageView(ListView):
     template_name="pages/main.html"
+    model=Post
+    context_object_name='all_posts_list'
